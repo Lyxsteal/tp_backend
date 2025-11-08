b67d832712034353296ab70e5ee640de61b626a4
maxib39 <maximobrane@gmail.com>
2025-11-08 11:22:33 -0300

    se pasaron los endpoints

--- ms_solicitud/src/main/java/com/example/ms_solicitud/controller/TarifaController.java
+++ ms_solicitud/src/main/java/com/example/ms_solicitud/controller/TarifaController.java
@@ -4,18 +4,24 @@ import com.example.ms_solicitud.model.Tarifa;
 import com.example.ms_solicitud.service.TarifaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
-@RequestMapping("api/solicitudes/tarifas")
+@RequestMapping("api/v1/tarifas")
 @RequiredArgsConstructor
 public class TarifaController {
     private final TarifaService service;
     //get
+    @GetMapping
+    public ResponseEntity<List<Tarifa>> obtenerTarifas() {
+        return ResponseEntity.ok(service.obtenerTodasLasTarifas());
+    }
+    @GetMapping("/{isTarifa}")
+    public  ResponseEntity<Tarifa> obtenerTarifaPorId(Integer idTarifa){
+        return ResponseEntity.ok(service.obtenerTarifaPorId(idTarifa));
+    }
     //post
     @PostMapping
     public ResponseEntity<Tarifa> crearTarifa (@RequestBody Tarifa tarifa){
@@ -23,4 +29,14 @@ public class TarifaController {
         return ResponseEntity.ok(tarifa);
     }
     //put
+    @PutMapping("/{idTarifa}")
+    public ResponseEntity<Tarifa> actualizarTarifa(@PathVariable Integer idTarifa, @RequestBody Tarifa tarifa){
+        return ResponseEntity.ok(service.actualizarTarifa(idTarifa, tarifa));
+    }
+    //delete
+    @DeleteMapping("/{idTarifa}")
+    public ResponseEntity<Void> borrarTarifa(@PathVariable Integer idTarifa){
+        service.eliminarTarifa(idTarifa);
+        return ResponseEntity.noContent().build();
+    }
 }
