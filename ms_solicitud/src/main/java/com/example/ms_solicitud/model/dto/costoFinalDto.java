b267f2cec43264f7ad52d6ae1f8fb161bc2daba0
maxib39 <maximobrane@gmail.com>
2025-11-16 00:29:43 -0300

    aaaa

---
+++ ms_solicitud/src/main/java/com/example/ms_solicitud/model/dto/costoFinalDto.java
@@ -0,0 +1,11 @@
+package com.example.ms_solicitud.model.dto;
+
+import lombok.Data;
+
+@Data
+public class costoFinalDto {
+    private Integer cantTramos;
+    private Float kmTotales;
+    private
+
+}
