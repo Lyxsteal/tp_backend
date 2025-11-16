86ad85757d25b9dec3f2d0fda950c8d5dbdc0ed0
Santiago Sammartino <ssammartino26@gmail.com>
2025-11-15 22:31:33 -0300

    Rehacer api gw

--- gateway/src/main/java/com/example/gateway/beans/GatewayBeans.java
+++ gateway/src/main/java/com/example/gateway/beans/GatewayBeans.java
@@ -1,4 +1,4 @@
-package backend.grupo73.gateway.beans;
+package com.example.gateway.beans;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
